92b16c4a981cda1caad03100de2ae3e3d6a5ea57
FortiShield <161459699+FortiShield@users.noreply.github.com>
2024-04-12 22:06:45 +0600

    Delete framework directory
    Signed-off-by: FortiShield <161459699+FortiShield@users.noreply.github.com>

--- framework/fortishield/core/cluster/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright (C) 2015, KhulnaSoft Ltd.
-# Created by KhulnaSoft, Ltd. <info@khulnasoft.com>.
-# This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
